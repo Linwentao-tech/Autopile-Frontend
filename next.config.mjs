0d90d201f5a2a4b25f25ef68f7e23cfa43245fa7
Max Lin <linwentao888@gmail.com>
2024-10-01 22:28:59 +1000

    fix building issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,15 @@
 /** @type {import('next').NextConfig} */
 import withPlaiceholder from "@plaiceholder/next";
 const nextConfig = {
-  experimental: {
-    turbotrace: {
-      memoryLimit: 3000,
-    },
-  },
-  output: "standalone",
   images: {
-    unoptimized: true,
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: "static.wixstatic.com",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    // ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
