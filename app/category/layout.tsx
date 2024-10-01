fd387c638e1d51f913c9142b2c19853e524edffb
Max Lin <linwentao888@gmail.com>
2024-10-01 22:11:07 +1000

    fixed building issue

--- app/category/layout.tsx
+++ app/category/layout.tsx
@@ -1,10 +1,13 @@
 import { ChildrenProps } from "@/app/components/InterfaceType";
 import ProductSideBar from "@/app/components/ProductSideBar";
+import { Suspense } from "react";
 
 function layout({ children }: ChildrenProps) {
   return (
     <div className="mx-12 flex justify-start gap-7">
-      <ProductSideBar />
+      <Suspense>
+        <ProductSideBar />
+      </Suspense>
       <main>{children}</main>
     </div>
   );
